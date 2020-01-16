82cb2b1f3a0dc941990c758eb139d099e473c68a
ringo2455 <iby2455@gmail.com>
2020-01-16 13:29:15 +0900

    Manual Pagination

--- config/authentication.py
+++ config/authentication.py
@@ -1,7 +1,6 @@
 import jwt
 from django.conf import settings
 from rest_framework import authentication
-from rest_framework import exceptions
 from users.models import User
 
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,23 @@
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from .models import Room
 from .serializers import RoomSerializer
 
 
+class RoomPagination(PageNumberPagination):
+    page_size = 20
+
+
 class RoomsView(APIView):
     def get(self, request):
-        rooms = Room.objects.all()[:5]
-        serializer = RoomSerializer(rooms, many=True)
-        return Response(serializer.data)
+        paginator = RoomPagination()
+        rooms = Room.objects.all()
+        results = paginator.paginate_queryset(rooms, request)
+        serializer = RoomSerializer(results, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         if not request.user.is_authenticated:
@@ -68,3 +76,11 @@ class RoomView(APIView):
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
+
+@api_view(["GET"])
+def room_search(request):
+    paginator = RoomPagination()
+    rooms = Room.objects.filter()
+    results = paginator.paginate_queryset(rooms, request)
+    serializer = RoomSerializer(results, many=True)
+    return paginator.get_paginated_response(serializer.data)
